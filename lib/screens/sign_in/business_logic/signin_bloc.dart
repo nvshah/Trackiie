8bbe99ff6df2e1580fc012b626536ab5bfc0630e
nvshah <nipunshah6776@gmail.com>
2021-01-22 22:37:39 +0530

    bug fix :  - bad state of disabling loading when error occured while signing in

--- lib/screens/sign_in/business_logic/signin_bloc.dart
+++ lib/screens/sign_in/business_logic/signin_bloc.dart
@@ -27,9 +27,18 @@ class SignInBloc {
       _setIsLoading(true);
       return await signInMethod();
     } catch (e) {
+      //Here we need to make this false as we're going to stay on same page &
+      // landing page ain't goign to replace the sign_in page i.e no rebuild of new screen
+      // So this catch bloc is always going to execute
+      _setIsLoading(false);
       rethrow;
     } finally {
-      _setIsLoading(false);
+      //As when sign in succeeds sign in page is going to be replaced by Home page So it's gonna to be disposed
+      // So no need to set SetIsLoading false then (In case signIn Succeeds)
+      // Another reason not to use below line is becuase ahead Landing page is rebuildin g the signIn page again on successful sign in,
+      // in which case this below line may not run earlier than that page build
+      // So page gets disposed & finally BLOC may or may not be executed
+      //_setIsLoading(false);
     }
   }
 
