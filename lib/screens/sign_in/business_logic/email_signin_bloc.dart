da470fd287a53da476d82d990e7010db26331867
nvshah <nipunshah6776@gmail.com>
2021-01-23 00:29:57 +0530

    Email Sign in bloc :  - submit form details

--- lib/screens/sign_in/business_logic/email_signin_bloc.dart
+++ lib/screens/sign_in/business_logic/email_signin_bloc.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
 
 import 'package:time_tracker/screens/sign_in/models/email_sign_in_model.dart';
+import 'package:time_tracker/services/auth.dart';
 
 class EmailSignInBloc {
+  final AuthBase auth;
+
+  EmailSignInBloc(this.auth);
+
   final StreamController<EmailSignInModel> _modelController =
       StreamController<EmailSignInModel>();
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
@@ -27,6 +32,24 @@ class EmailSignInBloc {
     );
     // Add model to stream
     _modelController.add(_model);
+  }
+
+  //Submit Form details
+  void submit() async {
+    updateWith(isLoading: true, isSubmitted: true);
+    try {
+      await ((_model.formType == EmailSignInFormType.signin)
+          ? auth.signInViaEmailAndPassword(
+              email: _model.email,
+              password: _model.password,
+            )
+          : auth.createUserViaEmailAndPassword(
+              email: _model.email,
+              password: _model.password,
+            ));
+    } catch (e) {} finally {
+      updateWith(isLoading: false);
+    }
   }
 
   void close() {
